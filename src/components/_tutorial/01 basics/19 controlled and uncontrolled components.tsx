7949289377a2e35af79d7600db5f1ca68930c9ff
Yobuligo <hoffmannpeter82@gmail.com>
2022-10-19 13:57:17 +0200

    Add example controlled vs uncontrolled components

---
+++ src/components/_tutorial/01 basics/19 controlled and uncontrolled components.tsx
@@ -0,0 +1,7 @@
+// Controlled vs uncontrolled components is an idiom.
+// Controlled components are components which are controlled via React. When using useState, React controls the components.
+// Uncontrolled components are components which are not controlled via React. When using useRef, you directly interact with the DOM and not with React which controls the DOM.
+
+export const ControlledAndUnControlledComponents = () => {
+  return <></>;
+};
