66cd719ccfb72d3a16d56c92c87fde65689b47e0
Yobuligo <hoffmannpeter82@gmail.com>
2022-10-21 17:10:24 +0200

    Add example useEffect Debouncing

--- src/components/_tutorial/01 basics/21 useEffect.tsx
+++ src/components/_tutorial/01 basics/21 useEffect.tsx
@@ -1,12 +1,13 @@
 // What does useEffect means and what is it for?
 // An effect is something that occurs by circumstances. E.g. when entering a value in the input value or when calling a REST service.
-// It has an effect on the application on a component.
+// It has an effect on the application on a component. So useEffects handle side effects which have an effect on a component
 // So it provides an easier way to update a component when the circumstances change. E.g. when a value is entered within an input field the input field changed and maybe a corresponding button will be enabled or disabled.
 //
 // A useEffect runs always when a component is called. So it is called for the first time. In addition it is called whenever a given dependency changed.
 // Dependencies must not be:
-//  1. the useState setter functions or other functions like setTimer.
-//  2. the global variables
+//  1. the state updating functions (the useState setter)
+//  2. build in APIs like localStorage
+//  3. other variables from outside the component or which are not bind to any component attribute
 //
 // Why use useEffect instead of useState?
 // 1. when useEffect is called you can be sure that depended variables really change and that these variables have to correct, current value
---
+++ src/components/_tutorial/01 basics/22 useEffect Debouncing.tsx
@@ -0,0 +1,43 @@
+// Bouncing means to hit, to collide (aufprallen)
+// Debouncing to avoid a bouncing.
+// Image an input field to enter a username. Whenever the content changed a REST call is send to check if the entered user is valid.
+// It would be pretty expensive to send the call for each entered letter. Instead it make sense to send it only if e.g. a certain time (e.g. 500 ms) past.
+// Therefore a timeout can be used. Whenever a value is entered to an input field the timer is set. Whenever a key is pressed within 500 ms (e.g. 300ms) the timer is cleared, reset.
+// That means the timer starts from the beginning. But only if the timer ends the REST call is executed. That is debouncing and avoid unnecessary traffic and UI-refreshes
+
+import { ChangeEvent, useEffect, useState } from "react";
+
+// here a button should be updated depending on the value within an input field. Whenever the value is longer than 6 characters the button is enabled.
+// but the update rendering only takes place if the user stops typing for longer than 500 ms
+export const UseEffectDebouncingComponent: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [disable, setDisable] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // this code only runs if the timer is up
+      // in that case it checks if the length of a value is greater or smaller than 6. Depended from that value it is decided if the button is enabled or disabled
+      if (value.length > 6) {
+        setDisable(false);
+      } else {
+        setDisable(true);
+      }
+    }, 500);
+
+    // the timer doesn't finish yet. Therefore it is cleared / reset. 
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value]);
+
+  const onInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <>
+      <input onChange={onInputChangeHandler} />
+      <button disabled={disable}>Click Me</button>
+    </>
+  );
+};
