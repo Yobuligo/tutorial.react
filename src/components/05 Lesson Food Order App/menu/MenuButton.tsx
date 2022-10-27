9ed54824ffc35986db5558be684fffcb9fa12e29
Yobuligo <hoffmannpeter82@gmail.com>
2022-10-27 22:43:59 +0200

    Introduce summary

--- src/components/05 Lesson Food Order App/menu/MenuButton.tsx
+++ src/components/05 Lesson Food Order App/menu/MenuButton.tsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+import { Context } from "../model/Context";
 import styles from "./MenuButton.module.css";
 
 export const MenuButton: React.FC = () => {
+  const ctx = useContext(Context)
+  const onShowSummaryClick = ()=>{
+    ctx.shoppingCartPositions.map((shoppingCartPosition) => {
+      return <></>
+    })
+  }
+
   return (
     <div className={styles.menuButton}>
-      <button>
+      <button onClick={onShowSummaryClick}>
         <div className={styles.menuButtonContent}>Your Card</div>
       </button>
     </div>
