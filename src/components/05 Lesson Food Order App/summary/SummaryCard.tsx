b14e4518531b47be884f82673d773e38f6bc3f48
Yobuligo <hoffmannpeter82@gmail.com>
2022-10-27 22:06:09 +0200

    Implement button for adding meal to shopping cart

--- src/components/05 Lesson Food Order App/summary/SummaryCard.tsx
+++ src/components/05 Lesson Food Order App/summary/SummaryCard.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import { Card } from "../../core/Card/Card";
 import { IMeal } from "../model/IMeal";
 import styles from "./SummaryCard.module.css";
 
-export const SummaryCard: React.FC<{ meal: IMeal }> = (props) => {
+export const SummaryCard: React.FC<{
+  meal: IMeal;
+  onAddMeal?: (meal: IMeal, amount: number) => void;
+}> = (props) => {
+  const [amount, setAmount] = useState<string>("0");
+  const onChangeEventHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(event.target.value);
+  };
+
+  const onAddMealEventHandler = () => {
+    console.log(`Add ${amount} of ${props.meal.title} to shopping cart.`);
+    props.onAddMeal?.(props.meal, Number(amount));
+  };
+
   return (
     <div className={styles.frame}>
       <Card>
@@ -15,10 +29,15 @@ export const SummaryCard: React.FC<{ meal: IMeal }> = (props) => {
           <div className={styles.control}>
             <div className={styles.input}>
               <label htmlFor="amount">Amount</label>
-              <input id="amount" type="number" />
+              <input
+                id="amount"
+                type="number"
+                onChange={onChangeEventHandler}
+              />
             </div>
-            <button>+</button>
-            <button>-</button>
+            <button type="button" onClick={onAddMealEventHandler}>
+              + Add
+            </button>
           </div>
         </div>
       </Card>
