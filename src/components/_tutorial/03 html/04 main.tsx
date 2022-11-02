7a7ed40c669bf5d17accff8f716862a42fe1f7df
Yobuligo <hoffmannpeter82@gmail.com>
2022-11-02 15:53:33 +0100

    Add example for main

---
+++ src/components/_tutorial/03 html/04 main.tsx
@@ -0,0 +1,10 @@
+// The main tag can be used to provide the main content of a page.
+// This is a content which don't repeat (repeating elements would be a navigation bar, copyright, etc.)
+
+export const MainComponent: React.FC = () => {
+  return (
+    <main>
+      <p>Any Main content of a page</p>
+    </main>
+  );
+};
