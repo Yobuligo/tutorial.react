29b0c91d310e3603a9bc4f0f08276cc23cdd69dd
Yobuligo <hoffmannpeter82@gmail.com>
2022-11-16 17:28:14 +0100

    Add example for custom hooks

---
+++ src/components/_tutorial/01 basics/35 custom hooks.tsx
@@ -0,0 +1,47 @@
+// Custom Hooks are used to encapsulate code of hooks which is required for certain components
+// The function name of a custom hook must start with "use" so that React recognize the function as custom hook.
+// For each component which uses that custom hook, a new instance of the custom hook is created.
+
+import { useEffect, useState } from "react";
+
+const useCounter = (operation: (previous: number) => number) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCounter((previous) => {
+        return operation(previous);
+      });
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [operation]);
+
+  return counter;
+};
+
+const CountUpComponent: React.FC = () => {
+  const counter = useCounter((previous) => {
+    return previous + 1;
+  });
+  return <>{counter}</>;
+};
+
+const CountDownComponent: React.FC = () => {
+  const counter = useCounter((previous) => {
+    return previous - 1;
+  });
+  return <>{counter}</>;
+};
+
+const CustomHooksComponent: React.FC = () => {
+  return (
+    <>
+      <CountUpComponent />
+      <CountDownComponent />
+    </>
+  );
+};
+
+export default CustomHooksComponent;
