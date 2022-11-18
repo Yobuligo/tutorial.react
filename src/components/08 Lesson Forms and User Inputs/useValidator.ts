2e6f131c29fee8cb2d510920cf02dfb225fc3be7
Yobuligo <hoffmannpeter82@gmail.com>
2022-11-18 10:03:43 +0100

    Lesson Forms and User Inputs: add reducer

--- src/components/08 Lesson Forms and User Inputs/useValidator.ts
+++ src/components/08 Lesson Forms and User Inputs/useValidator.ts
@@ -1,13 +1,57 @@
-import { useState } from "react";
+import { Reducer, useReducer } from "react";
+
+enum ValidatorAction {
+  VALUE_CHANGED,
+  BLUR,
+  RESET,
+}
+
+type IValidatorAction =
+  | { type: ValidatorAction.BLUR }
+  | { type: ValidatorAction.RESET }
+  | { type: ValidatorAction.VALUE_CHANGED; value: string };
+
+interface IState {
+  value: string;
+  valueTouched: boolean;
+}
+
+const validateReducer: Reducer<IState, IValidatorAction> = (
+  state: IState,
+  action: IValidatorAction
+) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case ValidatorAction.BLUR: {
+      newState.valueTouched = true;
+      break;
+    }
+    case ValidatorAction.RESET: {
+      newState.value = "";
+      newState.valueTouched = false;
+      break;
+    }
+    case ValidatorAction.VALUE_CHANGED: {
+      newState.value = action.value;
+      newState.valueTouched = true;
+      break;
+    }
+  }
+  return newState;
+};
 
 const useValidator = (validate: (value: string) => boolean) => {
-  const [value, setValue] = useState("");
-  const [valueTouched, setValueTouched] = useState(false);
+  const [validateState, dispatchValidateState] = useReducer(validateReducer, {
+    value: "",
+    valueTouched: false,
+  });
+  // const [value, setValue] = useState("");
+  // const [valueTouched, setValueTouched] = useState(false);
 
-  const isValueValid = validate(value);
+  const isValueValid = validate(validateState.value);
 
   const needsShowValueError = (): boolean => {
-    if (valueTouched) {
+    if (validateState.valueTouched) {
       if (isValueValid) {
         return false;
       } else {
@@ -18,26 +62,27 @@ const useValidator = (validate: (value: string) => boolean) => {
   };
 
   const onValueChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-    setValueTouched(true);
+    dispatchValidateState({
+      type: ValidatorAction.VALUE_CHANGED,
+      value: event.target.value,
+    });
   };
 
   const onValueBlurHandler = () => {
-    setValueTouched(true);
+    dispatchValidateState({ type: ValidatorAction.BLUR });
   };
 
   const reset = () => {
-    setValue("");
-    setValueTouched(false);
+    dispatchValidateState({ type: ValidatorAction.RESET });
   };
 
   return {
-    value,
+    value: validateState.value,
     isValueValid,
     onValueBlurHandler,
     onValueChangeHandler,
     needsShowValueError,
-    onSubmitted: reset
+    onSubmitted: reset,
   };
 };
 
