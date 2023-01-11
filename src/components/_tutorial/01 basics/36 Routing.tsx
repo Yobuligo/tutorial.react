db10558090287e042d8566b5511fff3ac20ef93e
Yobuligo <hoffmannpeter82@gmail.com>
2023-01-11 13:44:49 +0100

    Add chapter for routing

---
+++ src/components/_tutorial/01 basics/36 Routing.tsx
@@ -0,0 +1,8 @@
+// As a React Application is a single page application (SPA) there is no need to provide several HTML files for different pages which are loaded on demand.
+// Instead the whole application is transmitted to the client and by click events and input events the content of the page change.
+// But what if we want to use the possibility to navigate between pages by using /<name>? within the URL? 
+// Here routing comes to place.
+
+export const Routing: React.FC = ()=>{
+    return <></>
+}
