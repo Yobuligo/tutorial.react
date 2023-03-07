fbacb19b9b9d209c4f2c8a5b211455855209f26a
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-07 13:14:46 +0100

    Add chapter for Validation

---
+++ src/components/_tutorial/01 basics/37 Validation.tsx
@@ -0,0 +1,17 @@
+/**
+ * There are three ways to validate forms and entered values.
+ * 1. when a form was submitted
+ * 2. when an input loses focus
+ * 3. when a user entered a value
+ *
+ * Validation for the following input means to not being empty.
+ * 1. At the beginning the values of this form seems to be correct (even so nothing was entered)
+ * 2. When pressing the submit button it becomes invalid
+ * 3. when entering the input and directly leaving the input (losing focus), it becomes invalid
+ * 4. when entering a value the state changed from valid to invalid. Depending on the input
+ */
+const Validation: React.FC = () => {
+  return <></>;
+};
+
+export default Validation;
