f042039ea8a6441e4e8967e7ed24aaf974989a7c
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-10 09:37:24 +0100

    Extend documentation for useCallback and useMemo

--- src/components/_tutorial/01 basics/29 performance useCallback.tsx
+++ src/components/_tutorial/01 basics/29 performance useCallback.tsx
@@ -3,6 +3,9 @@
 // As mentioned in the previous chapter "performance react memo" it is possible to reduce the evaluation effort for rerendering a component by using React.memo.
 // Anyway that only works for scalar values but not for function or objects. In case of functions and objects each time the component function is called new instances are created. And this means for the React.memo that the component changed.
 // To also memories the state of functions and objects the hook "useCallback" can be used. So the functions and objects are cached and whenever a component is called the reference of a function or object is reused instead of creating a new instance.
+// Usage:
+// 1. if a component or hook depends of that function. The function is rerendered each time its own component is rerendered. In that time the depending components and hooks a rerendered. So instead use useCallback to avoid the rerendering.
+//
 // So React.memo can also be used for components with objects and functions.
 //
 // UseCallback works similar to useEffect. The function has to be provided, but also the dependencies which are leading to a change of a function
--- src/components/_tutorial/01 basics/30 performance useMemo.tsx
+++ src/components/_tutorial/01 basics/30 performance useMemo.tsx
@@ -2,6 +2,10 @@
 // It can be used for operations which might be expensive and should be executed only rarely. E.g. sorting of a list.
 // Therefore the useMemo hook is used. It has two parameters. The first is a function that does the work and returns the result. E.g. is sorts a list and returns the result.
 // The second parameter is a list of dependencies. As usually it means that the expensive function (to sort the items) only run, if any dependency change.
+// Usage:
+// 1. when e.g. a computation takes a long time and the result should be cached
+// 2. For reference equality. E.g. a useEffect should only be called when a reference changed. Then that reference should be wrapped by useMemo. So it wont be rerendered each time with the host component,
+//    but only when the whole component including the reference has to rebuild.
 
 import { useMemo } from "react";
 
