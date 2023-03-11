a59d52a34880ba88130d11cceef00cd9e1bbc68c
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-11 23:17:49 +0100

    Add styling for lession authentication

--- src/components/11 Lesson Authentication/components/Authentication.tsx
+++ src/components/11 Lesson Authentication/components/Authentication.tsx
@@ -11,23 +11,27 @@ const Authentication: React.FC = () => {
         }}
       >
         <div>
-          <div className={styles.element}>
-            <div>
-              <label htmlFor="email">E-Mail</label>
+          <div className={styles.footer}>
+            <div className={styles.element}>
+              <div>
+                <label htmlFor="email">E-Mail</label>
+              </div>
+              <div>
+                <input type="text" id="email" />
+              </div>
             </div>
-            <div>
-              <input type="text" id="email" />
+            <div className={styles.element}>
+              <div>
+                <label htmlFor="password">Password</label>
+              </div>
+              <div>
+                <input type="password" id="password" />
+              </div>
             </div>
           </div>
-          <div className={styles.element}>
-            <div>
-              <label htmlFor="password">Password</label>
-            </div>
-            <div>
-              <input type="password" id="password" />
-            </div>
+          <div className={styles.footer}>
+            <button>Login</button>
           </div>
-          <button>Login</button>
         </div>
       </form>
     </>
