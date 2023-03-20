234172de7c1e112572afdbf9aceec033da92f6de
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-20 14:13:05 +0100

    Extend example for lazy loading

--- src/components/_tutorial/01 basics/38 LazyComponent.tsx
+++ src/components/_tutorial/01 basics/38 LazyComponent.tsx
@@ -1,3 +1,7 @@
+export const sum = (value1: number, value2: number) => {
+  return value1 + value2;
+};
+
 const LazyComponent: React.FC = () => {
   return (
     <>
--- src/components/_tutorial/01 basics/38 lazy loading.tsx
+++ src/components/_tutorial/01 basics/38 lazy loading.tsx
@@ -7,14 +7,26 @@
  * To load a component lazy it is imported at the place where it is required.
  * Instead of importing the component directly it is imported by function *import*. The result is assigned to the variable e.g. the same as the component.
  * To display the component it is wrapped by *Suspense*. Here a fallback can be provided and as soon as the component was imported it can be displayed.
+ *
+ * Alternatively it is also possible to load e.g. a function lazy. Here the sum function is loaded on demand and the sum calculated.
  */
 
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useState } from "react";
 
 export const LazyLoadingComponent: React.FC = () => {
+  const [calcResult, setCalcResult] = useState(0);
+
+  // Lazy loading of a component
   const LazyComponent = lazy(() => import("./38 LazyComponent"));
+
+  // Lazy loading of a function
+  import("./38 LazyComponent").then((module) => {
+    setCalcResult(module.sum(4, 5));
+  });
+
   return (
     <>
+      {calcResult}
       <Suspense fallback={<p>... loading lazy component</p>}>
         <LazyComponent />
       </Suspense>
