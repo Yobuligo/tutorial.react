8c78e16e944ef82d219fe4eef10dc681a852f16a
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-28 14:33:14 +0200

    Extend lesson for useRef

--- src/components/_tutorial/01 basics/18 useRef.tsx
+++ src/components/_tutorial/01 basics/18 useRef.tsx
@@ -1,9 +1,13 @@
-// The function useRef() is an alternative to useState(). It means to directly refer to an HTML element of the DOM.
-// There is no need to have an additional variable as useState which keeps a value. Instead the DOM is accessed directly.
-// It would even be possible to change the DOM. But you shouldn't do that.
-// But partially it is easier to refer directly to the HTML element instead of introducing a useState
-// I would suggest it should only be used for reading reasons
-// the useRef has to be initialized by null
+/**
+ * The function useRef() is an alternative to useState(). It means to directly refer to an HTML element of the DOM.
+ * There is no need to have an additional variable as useState which keeps a value. Instead the DOM is accessed directly.
+ * It would even be possible to change the DOM. But you shouldn't do that.
+ * But partially it is easier to refer directly to the HTML element instead of introducing a useState
+ * I would suggest it should only be used for reading reasons
+ * the useRef has to be initialized by null
+ *
+ *  In addition useRef can be used to save values, which should not trigger an update.
+ */
 
 import { useRef } from "react";
 
@@ -17,10 +21,18 @@ export const UseRefComponent: React.FC = () => {
     }
   };
 
+  // If I would change toggle the component won't be rerendered
+  let toggle = useRef(true);
+
+  const onToggle = () => {
+    toggle.current = !toggle.current;
+  };
+
   return (
     <>
       <input ref={inputRef} />
       <button onClick={onButtonClickedHandler}>Click Me</button>
+      <button onClick={onToggle}>Toggle</button>
     </>
   );
 };
