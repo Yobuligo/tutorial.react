cd4b1dde1625fde05f33fd671c17533097ba58d0
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-28 09:03:38 +0200

    Extend lesson for error boundary

--- src/components/_tutorial/01 basics/40 Error Boundary.tsx
+++ src/components/_tutorial/01 basics/40 Error Boundary.tsx
@@ -1,33 +1,53 @@
-import { Component, ErrorInfo } from "react";
+import React, { ReactNode } from "react";
 
-// Bubbling errors or handle errors are hardly possible for functional components.
-// Instead you need a class and override method componentDidCatch(error){}. That class is a normal React component.
-// This means it can be embedded in other components and can write errors to a console or whatever is required to handle the error.
+/**
+ * Bubbling errors or handle errors are hardly possible for functional components.
+ * Instead you need a class and override method componentDidCatch(error){}. That class is a normal React component.
+ * This means it can be embedded in other components and can write errors to a console or whatever is required to handle the error.
+ */
 
-interface IErrorBoundaryComponent {
-  hasError: boolean;
-  children: React.ReactNode;
-}
-
-export class ErrorBoundaryComponent extends Component<
-  IErrorBoundaryComponent,
-  IErrorBoundaryComponent
+export class ErrorBoundary extends React.Component<
+  { children: ReactNode },
+  { hasError: boolean }
 > {
-  constructor() {
-    super({ hasError: false, children: [] });
-    this.state = { hasError: false, children: [] };
+  state = { hasError: false };
+
+  /**
+   * This method is called in case of an error. It can be used to recognize that an error occurs.
+   */
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.log(error);
-    this.setState({ hasError: true });
+  /**
+   * This method is also called in case of an error. This is normally used for logging the error and the errorInfos (call stack, etc.)
+   */
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.log(error, errorInfo);
   }
 
-  render() {
+  /**
+   * The render method simply decides if the children is rendered or in case of an error, what should be printed
+   */
+  render(): React.ReactNode {
     if (this.state.hasError) {
-      return <p>Something went wrong!</p>;
+      return <>Error occurred</>;
+    } else {
+      return this.props.children;
     }
-
-    return this.props.children;
   }
 }
+
+const ThrowingChildComponent: React.FC = () => {
+  throw new Error("Throwing an error for free");
+};
+
+const ErrorBoundaryComponent: React.FC = () => {
+  return (
+    <ErrorBoundary>
+      <ThrowingChildComponent />
+    </ErrorBoundary>
+  );
+};
+
+export default ErrorBoundaryComponent;
