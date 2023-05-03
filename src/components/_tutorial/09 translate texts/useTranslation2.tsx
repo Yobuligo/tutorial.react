628c75006c3a8284883bb20a72ef384b8c37ea0a
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-03 18:34:35 +0200

    Extend lesson for useTranslation2

--- src/components/_tutorial/09 translate texts/useTranslation2.tsx
+++ src/components/_tutorial/09 translate texts/useTranslation2.tsx
@@ -5,8 +5,9 @@
  * The problem hereby is that the texts must be in sync for all languages.
  *
  * But how to do it.
- *
- * Import the texts
+ * 1. see steps 1 - 3 from "useTranslation" hook.
+ * 2. import the texts via variable (here translations) from the folder texts.
+ * 3. return the text depending on the translation
  */
 
 import * as translations from "./texts";
@@ -14,20 +15,38 @@ import * as translations from "./texts";
 const useTranslation2 = () => {
   // language could be a separate hook
   const language: string = "en";
+
+  const i = (text: string, placeholders: any): string => {
+    let textCopy = text;
+    for (const placeholder in placeholders) {
+      textCopy = textCopy.replaceAll(
+        `{{${placeholder}}}`,
+        placeholders[placeholder]
+      );
+    }
+    return textCopy;
+  };
+
   switch (language) {
     case "de": {
-      return { t: translations["de"] };
+      return { t: translations["de"], i };
     }
     default: {
-      return { t: translations["en"] };
+      return { t: translations["en"], i };
     }
   }
 };
 
 /**
  * With this component it is possible to directly access the texts
+ * Via a separate text interpolation function placeholders can be replaced
  */
 export const UseTranslationComponent2: React.FC = () => {
-  const { t } = useTranslation2();
-  return <>{t.homePage.header}</>;
+  const { t, i } = useTranslation2();
+  return (
+    <>
+      <div>{t.homePage.header}</div>
+      <div>{i(t.sayHello, { name: "Peter" })}</div>
+    </>
+  );
 };
