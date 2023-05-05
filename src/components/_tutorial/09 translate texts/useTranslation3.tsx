42a178ef76299eca4f95a8b8b77a35c1ae1d34e7
Peter <hoffmannpeter82@gmail.com>
2023-05-05 10:43:50 +0200

    start third version of useTranslation

---
+++ src/components/_tutorial/09 translate texts/useTranslation3.tsx
@@ -0,0 +1,9 @@
+/**
+ * This version of the translation should cover more features that version 1 and 2
+ * Requirements:
+ *      texts should be accessed type safe
+ *      support default values
+ *      support replace of placeholders
+ *      support pluralization
+ *      support fallback values
+ */
