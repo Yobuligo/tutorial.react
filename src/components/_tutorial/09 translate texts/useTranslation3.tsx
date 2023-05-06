b7bb4eddaee4ac47a94486a97b92b250673d620c
Peter <hoffmannpeter82@gmail.com>
2023-05-06 13:58:20 +0200

    add interpolation for translation 3

--- src/components/_tutorial/09 translate texts/useTranslation3.tsx
+++ src/components/_tutorial/09 translate texts/useTranslation3.tsx
@@ -7,3 +7,19 @@
  *      support pluralization
  *      support fallback values
  */
+
+import { useLocalStorage } from "../08 custom hooks/useLocalStorage";
+import * as translations from "./texts";
+
+
+export const t = ()=>{
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [language, setLanguage] = useLocalStorage("language", "en")
+
+  const i = (text:string, placeholders: string[])=>{}
+
+}
+
+const useTranslation3 = () => {
+  return { t: translations["en"] };
+};
