509bfa04140baccb5bfa25ba488e83a49bfed1bb
Peter <hoffmannpeter82@gmail.com>
2023-05-07 19:34:43 +0200

    Add lesson for color picker

---
+++ src/components/_tutorial/03 html/09 build in color picker.tsx
@@ -0,0 +1,11 @@
+/**
+ * By choosing "color" as type for input, the input is converted to a color picker
+ */
+
+const BuildInColorPickerComponent: React.FC = () => {
+  return (
+    <>
+      <input type="color" />
+    </>
+  );
+};
