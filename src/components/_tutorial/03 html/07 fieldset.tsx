720ce38716df139b4993fb6797ace020d344a791
Peter <hoffmannpeter82@gmail.com>
2023-05-07 19:25:16 +0200

    Add lesson for fieldset

---
+++ src/components/_tutorial/03 html/07 fieldset.tsx
@@ -0,0 +1,12 @@
+/**
+ * Provides a frame around some tags. By providing a legend the fieldset gets a name.
+ */
+
+const FieldsetComponent: React.FC = () => {
+  return (
+    <fieldset>
+      <legend>Name of the Fieldset</legend>
+      <p>Content</p>
+    </fieldset>
+  );
+};
