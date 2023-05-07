b9fa82ac79ce2fc13dc119729b482f21343a6e39
Peter <hoffmannpeter82@gmail.com>
2023-05-07 19:37:42 +0200

    Add lesson for time picker

---
+++ src/components/_tutorial/03 html/11 build in time picker.tsx
@@ -0,0 +1,11 @@
+/**
+ * By choosing "time" as type for input, the input is converted to a time picker
+ */
+
+const BuildIntTimePicker: React.FC = () => {
+  return (
+    <>
+      <input type="time" />
+    </>
+  );
+};
