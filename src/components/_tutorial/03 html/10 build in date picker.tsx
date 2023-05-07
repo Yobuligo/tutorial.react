37cd9b945adddd3d0c7eec78b8effb2ddf0e2163
Peter <hoffmannpeter82@gmail.com>
2023-05-07 19:36:50 +0200

    Add lesson for date picker

---
+++ src/components/_tutorial/03 html/10 build in date picker.tsx
@@ -0,0 +1,11 @@
+/**
+ * By choosing "date" as type for input, the input is converted to a date picker
+ */
+
+const BuildInDatePicker: React.FC = () => {
+  return (
+    <>
+      <input type="date" />
+    </>
+  );
+};
