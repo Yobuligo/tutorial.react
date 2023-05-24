3cbdd842ac4031ce9efac59420a41f5dbca799b4
Peter <hoffmannpeter82@gmail.com>
2023-05-24 09:16:56 +0200

    Add lesson for useLanguage

--- src/components/_tutorial/08 custom hooks/useDataAccessObject.ts
+++ src/components/_tutorial/08 custom hooks/useDataAccessObject.ts
@@ -1,3 +1,23 @@
 /**
- * Find the corresponding code in chapter "*useDataAccessObject" in the basics sections
+ * Find the whole example in chapter "*useDataAccessObject" in the basics sections
  */
+
+import { useState } from "react";
+
+export const useDataAccessObject = <T>() => {
+  const [dataObjects, setDataObjects] = useState<T[]>([]);
+
+  const onAdd = (dataObject: T) => {
+    setDataObjects((previous) => [...previous, dataObject]);
+  };
+
+  const onDelete = (dataObject: T) => {
+    setDataObjects((previous) => {
+      const index = previous.findIndex((element) => element === dataObject);
+      previous.splice(index, 1);
+      return [...previous];
+    });
+  };
+
+  return { dataObjects, onAdd, onDelete };
+};
