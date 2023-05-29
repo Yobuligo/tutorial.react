ce86cf9890855a64dc30b97102984dd9b55aba2e
Peter <hoffmannpeter82@gmail.com>
2023-05-29 23:01:14 +0200

    Extend lesson for Custom Hooks

--- src/components/_tutorial/01 basics/35 custom hooks.tsx
+++ src/components/_tutorial/01 basics/35 custom hooks.tsx
@@ -1,7 +1,19 @@
-// Custom Hooks are used to encapsulate code of hooks which is required for certain components
-// The function name of a custom hook must start with "use" so that React recognize the function as custom hook.
-// For each component which uses that custom hook, a new instance of the custom hook is created.
-// The location of custom hooks is normally a separate folder with name 'hooks'
+/**
+ * Custom Hooks are used to encapsulate code of hooks which is required for certain components
+ * The function name of a custom hook must start with "use" so that React recognize the function as custom hook.
+ * For each component which uses that custom hook, a new instance of the custom hook is created.
+ * The location of custom hooks is normally a separate folder with name 'hooks'.
+ * 
+ * Custom Hooks might be:
+ *  - useDataAccessObject
+ *  - useLanguage
+ *  - useLocalStorage
+ *  - useTranslation
+ * 
+ * Best Practices:
+ * With each call of a custom hook that returns a function, that function is recreated. Which means in case the user e.g. caches the function in an array, it is added multiple times, as it recreated.
+ * To ensure it won't be recreated, the hook "useCallback" can be used.
+ */
 
 import { useEffect, useState } from "react";
 
