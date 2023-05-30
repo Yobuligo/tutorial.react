bac4eb84317208ee1f7208c554f57634e0495c46
Peter <hoffmannpeter82@gmail.com>
2023-05-30 11:38:54 +0200

    Update comments of react memo

--- src/components/_tutorial/01 basics/28 performance react memo.tsx
+++ src/components/_tutorial/01 basics/28 performance react memo.tsx
@@ -1,12 +1,14 @@
-// Each time a component state changed it has to be evaluated if a rerendering is required.
-// If you have complex applications with many components the evaluation becomes expensive.
-// Therefore it is possible to e.g. exclude a component from reevaluation or restrict the reevaluation.
-// To do so a component can be exported via React.memo. This means that a component is only reevaluation if a provided property changed. If the property not changed there is no need to reevaluate it.
-// Closures remember the values of variables from previous renders - this can help prevent async bugs
-//
-// Instead of reevaluating it, the depended properties are cached by React. In case a reevaluation is triggered, it is checked if the depended props changed. So keep in mind: this approach needs more memory!
-//
-// It is implemented as follows:
+/**
+ * Each time a component state changed it has to be evaluated if a rerendering is required.
+ * If you have complex applications with many components the evaluation becomes expensive.
+ * Therefore it is possible to e.g. exclude a component from reevaluation or restrict the reevaluation.
+ * To do so a component can be exported via React.memo. This means that a component is only reevaluation if a provided property changed. If the property not changed there is no need to reevaluate it.
+ * Closures remember the values of variables from previous renders - this can help prevent async bugs
+ *
+ * Instead of reevaluating it, the depended properties are cached by React. In case a reevaluation is triggered, it is checked if the depended props changed. So keep in mind: this approach needs more memory!
+ *
+ * It is implemented as follows:
+ */
 
 import React from "react";
 
