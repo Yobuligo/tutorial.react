4a826b89403d43c80af1129ec3f24b68b843c4a3
Peter <hoffmannpeter82@gmail.com>
2023-06-08 18:40:53 +0200

    Extend lesson for embedding images into React

--- src/components/_tutorial/03 html/01 embedd an image.tsx
+++ src/components/_tutorial/03 html/01 embedd an image.tsx
@@ -1,7 +1,14 @@
-// Probably there are several ways to embed an image into html
-// The easiest way is to use an url
-// but alternatively it is possible to provide it locally and use it as a resource. Therefore the local file has to be imported and assigned to an alias (here "placeholderImage")
-// Finally the alias can be used within an expression in the src property of the img tag.
+/**
+ * Probably there are several ways to embed an image into html
+ * The easiest way is to use an url
+ * but alternatively it is possible to provide it locally and use it as a resource. Therefore the local file has to be imported and assigned to an alias (here "placeholderImage")
+ * Finally the alias can be used within an expression in the src property of the img tag.
+ * 
+ * If the file won't be displayed or you get the error "Cannot find module 'file-name.png' or its corresponding type declarations".
+ * Check if the file "react-app-env.d.ts" exists in your project.
+ * It must contain "/// <reference types="react-scripts" />"
+ * Also see https://stackoverflow.com/questions/71099924/cannot-find-module-file-name-png-or-its-corresponding-type-declarations-type
+ */
 
 import placeholderImage from "./placeholder.svg";
 
