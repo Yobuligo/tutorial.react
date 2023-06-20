d4273bd61bb9637f01f509b63b554189d11f56da
Peter <hoffmannpeter82@gmail.com>
2023-06-20 09:50:07 +0200

    Add documentation to readLocalStorage and writeLocalStorage

--- src/components/_tutorial/11 utils/readLocalStorage.ts
+++ src/components/_tutorial/11 utils/readLocalStorage.ts
@@ -1,3 +1,7 @@
+/**
+ * The following function is a service function that provides the possibility to read from the local storage and returns a deserialized item, that data in form of T
+ */
+
 export const readLocalStorage = <T>(key: string): T | undefined => {
   const item = localStorage.getItem(key);
   if (item) {
--- src/components/_tutorial/11 utils/writeLocalStorage.ts
+++ src/components/_tutorial/11 utils/writeLocalStorage.ts
@@ -1,3 +1,7 @@
+/**
+ * The following function is a service function that provides the possibility to write to the local storage and returns data
+ */
+
 export const writeLocalStorage = <T>(key: string, data: T): T => {
   const item = JSON.stringify(data);
   localStorage.setItem(key, item);
