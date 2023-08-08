203fcdb7045069d0e89500d9f90979df86d1198c
Peter <hoffmannpeter82@gmail.com>
2023-08-08 16:49:15 +0200

    Rename backdrop to modalBackdrop

--- src/components/_tutorial/01 basics/15.1 Modal dialog with close.tsx
+++ src/components/_tutorial/01 basics/15.1 Modal dialog with close.tsx
@@ -31,7 +31,7 @@ namespace ModalDialogWithClose {
       <>
         {ReactDOM.createPortal(
           <div className={styles.modalContainer}>
-            <div className={styles.backdrop} onClick={props.onClose} />
+            <div className={styles.modalBackdrop} onClick={props.onClose} />
             <div className={styles.modalDialog}>{props.children}</div>
           </div>,
           document.getElementById("modalContainer")!
