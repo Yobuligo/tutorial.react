eda4c58b73cfabc79b3c05c14da434d9dee4766f
Yobuligo <hoffmannpeter82@gmail.com>
2023-08-25 23:12:26 +0200

    Remove polling namespace

--- src/components/_tutorial/11 utils/poll.ts
+++ src/components/_tutorial/11 utils/poll.ts
@@ -3,44 +3,42 @@
  * But actually it is enough to just have a method for polling.
  */
 
-namespace Polling {
-  interface IRepository<T> {
-    findAll(): Promise<T[]>;
-    version(id: number): Promise<Date>;
-  }
-
-  interface IPerson {
-    firstname: string;
-    age: number;
-  }
-
-  /**
-   * More or less a technical function that shouldn't accessible from outside, as it has a parameter lastVersion
-   */
-  const startPoll = <T>(
-    lastVersion: Date | undefined,
-    repository: IRepository<T>,
-    id: number,
-    onChange: () => void
-  ) => {
-    setTimeout(async () => {
-      const version = await repository.version(id);
-      if (lastVersion !== version) {
-        lastVersion = version;
-        await onChange();
-      }
-      startPoll(lastVersion, repository, id, onChange);
-    }, 1000);
-  };
-
-  /**
-   * The actually poll function, that is called form outside.
-   */
-  const poll = <T>(
-    repository: IRepository<T>,
-    id: number,
-    onChange: () => void
-  ) => {
-    startPoll(undefined, repository, id, onChange);
-  };
+interface IRepository<T> {
+  findAll(): Promise<T[]>;
+  version(id: number): Promise<Date>;
 }
+
+interface IPerson {
+  firstname: string;
+  age: number;
+}
+
+/**
+ * More or less a technical function that shouldn't accessible from outside, as it has a parameter lastVersion
+ */
+const startPoll = <T>(
+  lastVersion: Date | undefined,
+  repository: IRepository<T>,
+  id: number,
+  onChange: () => void
+) => {
+  setTimeout(async () => {
+    const version = await repository.version(id);
+    if (lastVersion !== version) {
+      lastVersion = version;
+      await onChange();
+    }
+    startPoll(lastVersion, repository, id, onChange);
+  }, 1000);
+};
+
+/**
+ * The actually poll function, that is called form outside.
+ */
+const poll = <T>(
+  repository: IRepository<T>,
+  id: number,
+  onChange: () => void
+) => {
+  startPoll(undefined, repository, id, onChange);
+};
