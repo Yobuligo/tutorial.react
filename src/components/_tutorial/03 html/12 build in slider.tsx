4ad12c18499ad229eb68652991447d906ed66a37
yobuligo <hoffmannpeter82@gmail.com>
2023-09-03 17:34:22 +0200

    Extend lesson for slider by showing how to set color

--- src/components/_tutorial/03 html/12 build in slider.tsx
+++ src/components/_tutorial/03 html/12 build in slider.tsx
@@ -1,11 +1,14 @@
 /**
- * By choosing "time" as type for input, the input is converted to a slider
+ * By choosing "time" as type for input, the input is converted to a slider.
+ * The easiest way to change the color of the slider is to set the property "accent-color".
  */
+
+import styles from "./12 build in slider.module.css";
 
 export const BuildIntTimePicker: React.FC = () => {
   return (
     <>
-      <input type="range" />
+      <input type="range" className={styles.slider} />
     </>
   );
 };
