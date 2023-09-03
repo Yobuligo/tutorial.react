28433d65ebf4655f251ae66535911d8dc09eed72
yobuligo <hoffmannpeter82@gmail.com>
2023-09-03 15:31:23 +0200

    Add lesson for implementing a slider

---
+++ src/components/_tutorial/03 html/12 build in slider.tsx
@@ -0,0 +1,11 @@
+/**
+ * By choosing "time" as type for input, the input is converted to a slider
+ */
+
+export const BuildIntTimePicker: React.FC = () => {
+  return (
+    <>
+      <input type="range" />
+    </>
+  );
+};
