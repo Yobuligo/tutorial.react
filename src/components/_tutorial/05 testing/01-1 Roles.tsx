b9a7bf3295ceca3a0816ef8115425877b8c06a95
Yobuligo <hoffmannpeter82@gmail.com>
2024-01-03 09:27:14 +0100

    Add roles

---
+++ src/components/_tutorial/05 testing/01-1 Roles.tsx
@@ -0,0 +1,13 @@
+/**
+ * Roles are used to identify elements in a react application.
+ * Normally those roles shouldnt be set manually.
+ * 
+ * The following roles are default:
+ * heading -> h1 - h6
+ * list -> ul, li
+ * button -> button
+ * link -> a
+ * textbox -> input, type="text"
+ * 
+ * full list: https://www.w3.org/TR/html-aria/#docconformance
+ */
