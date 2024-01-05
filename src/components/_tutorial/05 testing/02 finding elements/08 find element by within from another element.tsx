ea2469e2d6e2fbf8bb33b3d901938ee901188460
Yobuligo <hoffmannpeter82@gmail.com>
2024-01-05 16:42:53 +0100

    Add lesson for testing: implement custom matchers

--- src/components/_tutorial/05 testing/02 finding elements/08 find element by within from another element.tsx
+++ src/components/_tutorial/05 testing/02 finding elements/08 find element by within from another element.tsx
@@ -1,6 +1,8 @@
 /**
  * The method within doesn't only work with data-testid, but also with other elements.
  * So it is possible to find an element and to search for other elements within that form with the method within.
+ *
+ * Consider that we specify the aria-label in the form as search term.
  */
 
 import { render, screen, within } from "@testing-library/react";
@@ -9,7 +11,7 @@ const Test: React.FC = () => {
   return (
     <>
       <button>Go Back</button>
-      <form>
+      <form aria-label="form">
         <button>Submit</button>
         <button>Cancel</button>
       </form>
---
+++ src/components/_tutorial/05 testing/03-1 custom matchers/01 custom matchers.tsx
@@ -0,0 +1,51 @@
+/**
+ * If we have to check the same behavior several times, we can create own custom matchers.
+ *
+ * Imagine, we have a form with 2 buttons and we have that scenario very often and also the tests.
+ * To not have duplicate code, we can implement an own custom matcher.
+ */
+
+import { ByRoleMatcher, render, screen, within } from "@testing-library/react";
+
+const Test: React.FC = () => {
+  return (
+    <>
+      <button>Go Back</button>
+      <form aria-label="form">
+        <button>Submit</button>
+        <button>Cancel</button>
+      </form>
+    </>
+  );
+};
+
+// This is our custom matcher. It must return an instance of ?CustomMatcherResult?, which has the two props pass and message.
+const toContainRole = (
+  container: HTMLElement,
+  role: ByRoleMatcher,
+  quantity: number
+) => {
+  const elements = within(container).queryAllByRole(role);
+
+  if (elements.length === quantity) {
+    return {
+      pass: true,
+      message: () => "",
+    };
+  }
+
+  return {
+    pass: false,
+    message: () =>
+      `Expect to find ${quantity} ${role} elements, but found ${elements.length}`,
+  };
+};
+
+// The custom matcher has to be provided to the expect function by using method extend.
+expect.extend({ toContainRole });
+
+test("only find buttons within the form", () => {
+  render(<Test />);
+  const form = screen.getByRole("form");
+  expect(form).toContainRole("button", 2);
+});
