180ee9d04c3a79bf6839d124bde5c37e251f0f89
Yobuligo <hoffmannpeter82@gmail.com>
2024-01-07 23:17:48 +0100

    Extend matchers

--- src/components/_tutorial/05 testing/03 matchers/01 matchers.tsx
+++ src/components/_tutorial/05 testing/03 matchers/01 matchers.tsx
@@ -3,7 +3,7 @@
  * There are a lot matchers, but here are some more important:
  * - expect(["a", "b"]).toHaveLength(2) -> Array has a specific number of elements
  * - expect(5).toEqual(5)
- * - expected("aber").toContain("b") 
+ * - expected("aber").toContain("b")
  * - expect(fn).toThrow() -> Does function throws exception?
  * - expect(mock).toHaveBeenCalled() -> Was mock function called?
  * - expect(element).toBeInTheDocument() -> element in document?
@@ -11,4 +11,5 @@
  * - expect(element).toHaveClass() -> Does element has a class name?
  * - expect(element).toHaveTextContent() -> Does element have particular text?
  * - expect(element).toHaveValue() -> Does input, select or textarea specific value?
+ * - expect(element).toHaveAttribute("href", "/my-url") -> checks if element has property href with value "/my-url"
  */
