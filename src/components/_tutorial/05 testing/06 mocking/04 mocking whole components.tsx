e3417867e8e48ea039b25dc62d6449aba487aa71
Yobuligo <hoffmannpeter82@gmail.com>
2024-01-07 23:07:51 +0100

    Add lesson for testing: mock a whole component

---
+++ src/components/_tutorial/05 testing/06 mocking/04 mocking whole components.tsx
@@ -0,0 +1,41 @@
+/**
+ * If we are not able to render a sub component, it is possible to mock the whole one.
+ * This is possible by function jest.mock
+ *
+ * Attention: It is required to use the default export keywords (maybe find another solution later)
+ * Here comes how it works
+ */
+
+import { render } from "@testing-library/react";
+
+/**
+ * We want to mock this component
+ */
+const Callee: React.FC = () => {
+  return <>My content</>;
+};
+
+/**
+ * This is the component under test and we want to mock component {@link Callee}.
+ * Mocking means we want to replace it by another component.
+ */
+const Caller: React.FC = () => {
+  return <Callee />;
+};
+
+/**
+ * To mock the component {@link Callee}, we have to use the function jest.mock.
+ * It requires two parameters.
+ * 1. the path to the component that should be mocked
+ * 2. a function that returns an alternative component, which should be rendered instead
+ */
+jest.mock("./Callee", () => {
+  return () => {
+    return <>Alternative component, that is rendered instead of the origin.</>;
+  };
+});
+
+test("this test will not render component callee, but the alternative instead", () => {
+  // As we defined before jest.mock, Caller is rendered without rendering component Callee.
+  render(<Caller />);
+});
